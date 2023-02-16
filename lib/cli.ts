9ff759f81cf1a756143bc3972a995a18e59ebb77
pengbo43 <pengbo43@jd.com>
2023-02-16 20:44:54 +0800

    init: init

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,16 +1,43 @@
 import { cac } from 'cac'
+import prompts from 'prompts'
+import { execaCommandSync } from 'execa'
+import type { PromptObject } from 'prompts'
 
 const cli = cac('fe-weki')
 
+const frameWorkChoices = [
+  {
+    title: 'vue',
+    value: ' npm init vue@latest',
+  },
+  {
+    title: 'taro',
+    value: 'npx @tarojs/cli init',
+  },
+]
+
+const FRAMEWORK: PromptObject[] = [
+  {
+    type: 'select',
+    name: 'init',
+    message: '请选择需要使用的框架！',
+    choices: frameWorkChoices,
+  },
+]
+
 // 收集 前端框架 自动化创建项目
 
-cli.option('test', 'test')
+cli.option('[appName]', 'the target app name')
 
 cli
-  .command('[root]', 'start') // default command
-  .alias('start')
-  .action(async (root, options) => {
-    console.log(root, options)
+  .command('[appName]', 'init')
+  .alias('init')
+  .action(async (appName: string) => {
+    const answers = await prompts(FRAMEWORK)
+    const { init } = answers
+    execaCommandSync(`${init} ${appName || ''}`, {
+      stdio: 'inherit',
+    })
   })
 
 cli.help()
